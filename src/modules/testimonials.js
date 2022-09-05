55fbd45f2b95dc2bae859330e4e1c8708315b146
Danila Avetisyan <avetisyandanila21@yandex.ru>
2022-09-05 22:49:17 +0300

    Сделал блок "Testimonials"

--- src/modules/testimonials.js
+++ src/modules/testimonials.js
@@ -1,5 +1,10 @@
-import Swiper from 'swiper';
+import Swiper, {Navigation} from 'swiper';
+Swiper.use([Navigation]);
 
-const swiper = new Swiper('.swiper', {
-  loop: true,
+const swiper = new Swiper('.swiper--testimonials', {
+  spaceBetween: 60,
+  navigation: {
+    nextEl: '.swiper--testimonials .swiper-button--next',
+    prevEl: '.swiper--testimonials .swiper-button--prev',
+  },
 })
