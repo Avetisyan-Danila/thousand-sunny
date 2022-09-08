a1e4d6912827b1d78e79d45d3bca444fafdc6612
Danila Avetisyan <avetisyandanila21@yandex.ru>
2022-09-08 20:15:22 +0300

    Сделал адаптив >992

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -2,6 +2,17 @@ import Swiper from 'swiper';
 
 const swiper = new Swiper('.swiper--cards', {
   spaceBetween: 25,
-  slidesPerView: 4,
+  slidesPerView: 1,
   grabCursor: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
 })
