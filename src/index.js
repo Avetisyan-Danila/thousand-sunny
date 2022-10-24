3ceaab02899b2c18ba7eb06b9473d06b376345ac
Danila Avetisian <avetisyandanila21@yandex.ru>
2022-10-24 18:57:10 +0400

    Рефакторинг /libs
    * Перенёс slim-select

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './sass/index.scss';
-import './modules/slim-select'
+import './libs/js/slim-select'
 import './modules/testimonials'
 import './modules/cards'
 import './modules/header'
